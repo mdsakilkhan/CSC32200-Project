220a657d25fc85ed7c3bddcf59b6de47b2d1bff5
jchang005 <joshua.chang998@gmail.com>
2021-04-10 20:44:38 -0400

    started experimenting with xml and creating a customer class

---
+++ Customer.py
@@ -0,0 +1,9 @@
+class Customer():
+    def __init__(self):
+        self.first_name = ''
+        self.last_name = ''
+        self.date_of_birth = ''
+
+        self.balance = 0
+        self.card_number = 0
+        self.purchases = []
