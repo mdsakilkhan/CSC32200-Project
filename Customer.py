efda06e513bb2a6fc053f12b0046d73c19545277
jchang005 <joshua.chang998@gmail.com>
2021-04-10 23:58:22 -0400

    created users.py file containing all classes defining all users

--- Customer.py
+++
@@ -1,9 +0,0 @@
-class Customer():
-    def __init__(self):
-        self.first_name = ''
-        self.last_name = ''
-        self.date_of_birth = ''
-
-        self.balance = 0
-        self.card_number = 0
-        self.purchases = []
---
+++ Users.py
@@ -0,0 +1,27 @@
+
+
+class DeliveryCompany():
+    pass
+
+
+class ComputerPartCompany():
+    pass
+
+
+class StoreClerk():
+    pass
+
+
+class Customer():
+    def __init__(self):
+        self.first_name = ''
+        self.last_name = ''
+        self.contact_num = ''
+        self.email = ''
+        self.address = ''
+        self.payment_info = []
+        self.date_of_birth = ''
+
+        self.balance = 0
+        self.card_number = 0
+        self.purchases = []
