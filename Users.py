642cda11f0284da6159c39dc8010ff0c79ca9a9e
jchang005 <joshua.chang998@gmail.com>
2021-04-13 00:20:56 -0400

    "Branch test and updates to Users.py"

--- Users.py
+++ Users.py
@@ -8,20 +8,25 @@ class ComputerPartCompany():
     pass
 
 
+class Person():
+    def __init__(self, **kwargs):
+        self.first_name = kwargs['first_name']
+        self.last_name = kwargs['last_name']
+        self.date_of_birth = kwargs['date_of_birth']
+        self.contact_num = kwargs['contact_num']
+        self.email = kwargs['email']
+        self.address = kwargs['address']
+
+
 class StoreClerk():
-    pass
+    def __init__(self, **kwargs):
+        super.__init__(self, **kwargs)
 
 
-class Customer():
-    def __init__(self):
-        self.first_name = ''
-        self.last_name = ''
-        self.contact_num = ''
-        self.email = ''
-        self.address = ''
-        self.payment_info = []
-        self.date_of_birth = ''
+class Customer(Person):
+    def __init__(self, **kwargs):
+        Person.__init__(self, **kwargs)
 
-        self.balance = 0
-        self.card_number = 0
-        self.purchases = []
+        self.balance = kwargs['balance']
+        self.card_number = kwargs['card_number']
+        self.purchases = kwargs['purchases']
