f52e13df6baf8f3993b413a3f205f90113ab37d8
jchang005 <joshua.chang998@gmail.com>
2021-04-16 23:58:46 -0400

    "Switched from using **kwargs"

--- Users.py
+++ Users.py
@@ -1,5 +1,6 @@
-
-
+# Major overhaul in constructor logic:
+# I switched from using **kwargs to what it is now.
+# Check out this stackoverflow post: https://stackoverflow.com/questions/9728243/is-self-dict-updatekwargs-good-or-poor-style
 class DeliveryCompany():
     pass
 
@@ -9,37 +10,37 @@ class ComputerPartCompany():
 
 
 class Person():
-    def __init__(self, **kwargs):
-        self.first_name = kwargs['first_name']
-        self.last_name = kwargs['last_name']
-        self.date_of_birth = kwargs['date_of_birth']
-        self.contact_num = kwargs['contact_num']
-        self.email = kwargs['email']
-        self.address = kwargs['address']
+    def __init__(self, first_name=None, last_name=None, date_of_birth=None, contact_num=None, email=None, address=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+        self.contact_num = contact_num
+        self.email = email
+        self.address = address
 
 
 class StoreClerk():
-    def __init__(self, **kwargs):
-        super.__init__(self, **kwargs)
+    def __init__(self):
+        super().__init__(self)
 
 
 class Customer(Person):
-    def __init__(self, **kwargs):
-        super().__init__(self, **kwargs)
+    def __init__(self, card_number=None, bank=None, security_num=None, balance=None, purchase=None, cart=None, orders=None):
+        super().__init__(self)
         # Payment information
-        self.card_number = kwargs['card_number']
-        self.bank = kwargs['bank']
-        self.security_num = kwargs['security_num']
+        self.card_number = card_number
+        self.bank = bank
+        self.security_num = security_num
 
-        self.balance = kwargs['balance']
-        self.purchases = kwargs['purchases']
-        self.cart = []
-        self.orders = []
+        self.balance = balance
+        self.purchases = purchases
+        self.cart = cart
+        self.orders = orders
 
 
 class Order():
-    def __init__(self, **kwargs):
-        self.items = kwargs['items']
-        self.purchase_date = kwargs['purchase_date']
-        self.subtotal = kwargs['subtotal']
-        self.total = kwargs['total']
+    def __init__(self, items=None, purchase_date=None, subtotal=None, total=None):
+        self.items = items
+        self.purchase_date = purchase_date
+        self.subtotal = subtotal
+        self.total = total
--- xmlTest.py
+++ xmlTest.py
@@ -1,2 +1,11 @@
-from xml.dom.minidom import parse
 import xml.dom.minidom
+import sys
+import Users
+from datetime import datetime
+
+customer1 = Users.Person(first_name="Joshua",
+                         last_name="Chang",
+                         date_of_birth=datetime(month=4, day=16, year=2021),
+                         email='Joshua.chang998@gmail.com')
+
+print(customer1.date_of_birth)
