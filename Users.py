b74e85c6e36c3fed866927bee014879d564cdaf3
jchang005 <joshua.chang998@gmail.com>
2021-04-20 00:00:42 -0400

    made newUserForm.ui and additions to main.py

--- Users.py
+++ Users.py
@@ -9,12 +9,12 @@ class ComputerPartCompany():
 
 
 class Person():
-    def __init__(self, first_name=None, last_name=None, date_of_birth=None, contact_num=None, email=None, address=None):
+    def __init__(self, first_name=None, last_name=None, date_of_birth=None, contact_num=None, email_address=None, address=None):
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
         self.contact_num = contact_num
-        self.email = email
+        self.email_address = email_address
         self.address = address
 
 
--- main.py
+++ main.py
@@ -6,16 +6,34 @@ from PyQt5.uic import loadUi
 from LoginForm import Ui_LoginForm
 
 
-class StartUp(qtw.QWidget):
+class Main(qtw.QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.loginForm = LoginForm()
 
-        self.ui = Ui_LoginForm()
-        self.ui.setupUi(self)
-        self.show()
+
+class LoginForm(qtw.QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = loadUi("LoginForm.ui")
+
+        # Connect signals and slots
+        self.ui.pButton_newUser.clicked.connect(self.show_new_user_form)
+
+        self.ui.show()
+
+    def show_new_user_form(self):
+        newUserForm = NewUserForm()
+
+
+class NewUserForm(qtw.QDialog):
+    def __init__(self):
+        super().__init__()
+        page1 = loadUi("newUserForm.ui")
+        page1.show()
 
 
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
-    w = StartUp(windowTitle='Login')
+    w = Main(windowTitle='Login')
     app.exec_()
