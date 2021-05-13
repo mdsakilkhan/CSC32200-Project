0671e3a1096a920c6ee4ba677271004fdd55d517
Nana Bonsu <nanabonsu@Nanas-MBP.home>
2021-05-13 11:56:48 -0400

    More updates

--- Users.py
+++ Users.py
@@ -24,7 +24,7 @@ class StoreClerk(Person):
         super().__init__(**kwargs)
 
 class Customer(Person):
-    def __init__(self, password=None, card_number=None, bank=None, security_num=None, balance="0", is_manager=None, purchases=None, cart=None, orders=None, **kwargs):
+    def __init__(self, password=None, card_number=None, bank=None, security_num=None, balance="0", is_manager=False, purchases=None, cart=None, orders=None, **kwargs):
         super().__init__(**kwargs)
         self.password = password
         self.card_number = card_number
